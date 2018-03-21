e9a62a19dee167a4c666adba693173a87361d613
Vijendra Khemnar <vijendra.khemnar@gmail.com>
2018-03-21 15:25:48 +0530

    Source code for Multiple Env

---
+++ MultipleEnvApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultipleEnvApp
+//
+//  Created by Vijendra Khemnar on 05/03/18.
+//  Copyright © 2018 Talentica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
